8af50218b11dc7caec36fb8e2e8c96d379edb775
Jesper <94371018+mosua123@users.noreply.github.com>
2023-10-14 10:23:00 +0200

    Projektdateien hinzufügen.

---
+++ Consoletnsjesper/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Consoletnsjesper
+{
+    class Programm
+    {
+        static void Main() 
+        {
+
+            String name = "Jesper";
+            int age = 16;
+            String hobby = "Coden...";
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(hobby);
+            Console.ReadKey();
+
+        }
+    }
+}
